a0a823a892582a368b0d53e14ca47c8ef973b417
1565163445@qq.com <1565163445@qq.com>
2020-08-31 08:39:51 +0800

    02-type.ts 数字 字符串 数组 元组 枚举

--- 02-types.ts
+++ 02-types.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-08-31 07:44:01
- * @LastEditTime: 2020-08-31 08:12:46
+ * @LastEditTime: 2020-08-31 08:36:14
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \小卡车的博客f:\Github_my_project\TypeScriptLearn\typescript\02-types.ts
@@ -22,28 +22,59 @@
 
 // 数字类型
 var num: number = 12
-console.log(num)
+console.log(num)      // 12 
 
 // 字符串类型
 var str: string = "Hello World"
 str = "Hello 小卡车"
 
 // 数组类型
-let arr:number[] = [1,2,3,4,5,6,7,8,9]
-console.log(arr)
+let arr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(arr)      // [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-let arr1: Array<number> = [1,2,3,4,5,6,7,8,9]
-console.log(arr1)
+let arr1: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(arr1)        // [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 // 元组类型
-let arr2:[string, number, boolean] = ["小卡车", 18, true]
-console.log(arr2)
+let arr2: [string, number, boolean] = ["小卡车", 18, true]
+console.log(arr2)      // ["小卡车", 18, true]
 
 // 枚举类型
+// 有赋值的情况
 enum Xkc {
   name = '小卡车',
   age = 18
 }
 var name1: Xkc = Xkc.name
 var age: Xkc = Xkc.age
-console.log(`姓名：${name1}\t年龄：${age}`)
+console.log(`姓名：${name1}\t年龄：${age}`)    // 姓名：小卡车    年龄：18
+// 没有赋值的情况，默认值默认为下标值，从编译出来的js文件的可以看出
+enum Color {
+  red,
+  green,
+  blue
+}
+var red: Color = Color.red
+var green: Color = Color.green
+var blue: Color = Color.blue
+console.log(red)      // 0
+console.log(green)    // 1
+console.log(blue)     // 2
+// 部分赋值的情况，默认赋值的下一个开始每个递增加一
+enum Color1 {
+  red,
+  green = 6,
+  blue,
+  orange = 1,
+  black
+}
+var red1: Color1 = Color1.red
+var green1: Color1 = Color1.green
+var blue1: Color1 = Color1.blue
+var orange1: Color1 = Color1.orange
+var black1: Color1 = Color1.black
+console.log(red1)         // 0
+console.log(green1)       // 6
+console.log(blue1)        // 7
+console.log(orange1)      // 1
+console.log(black1)       // 2
--- js/02-types.js
+++ js/02-types.js
@@ -2,7 +2,7 @@
 /*
  * @Author: your name
  * @Date: 2020-08-31 07:44:01
- * @LastEditTime: 2020-08-31 08:12:46
+ * @LastEditTime: 2020-08-31 08:36:14
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \小卡车的博客f:\Github_my_project\TypeScriptLearn\typescript\02-types.ts
@@ -21,19 +21,20 @@
   */
 // 数字类型
 var num = 12;
-console.log(num);
+console.log(num); // 12 
 // 字符串类型
 var str = "Hello World";
 str = "Hello 小卡车";
 // 数组类型
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(arr);
+console.log(arr); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
 var arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(arr1);
+console.log(arr1); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
 // 元组类型
 var arr2 = ["小卡车", 18, true];
-console.log(arr2);
+console.log(arr2); // ["小卡车", 18, true]
 // 枚举类型
+// 有赋值的情况
 var Xkc;
 (function (Xkc) {
     Xkc["name"] = "\u5C0F\u5361\u8F66";
@@ -41,4 +42,36 @@ var Xkc;
 })(Xkc || (Xkc = {}));
 var name1 = Xkc.name;
 var age = Xkc.age;
-console.log("\u59D3\u540D\uFF1A" + name1 + "\t\u5E74\u9F84\uFF1A" + age);
+console.log("\u59D3\u540D\uFF1A" + name1 + "\t\u5E74\u9F84\uFF1A" + age); // 姓名：小卡车    年龄：18
+// 没有赋值的情况，默认值默认为下标值，从编译出来的js文件的可以看出
+var Color;
+(function (Color) {
+    Color[Color["red"] = 0] = "red";
+    Color[Color["green"] = 1] = "green";
+    Color[Color["blue"] = 2] = "blue";
+})(Color || (Color = {}));
+var red = Color.red;
+var green = Color.green;
+var blue = Color.blue;
+console.log(red); // 0
+console.log(green); // 1
+console.log(blue); // 2
+// 部分赋值的情况，默认赋值的下一个开始每个递增加一
+var Color1;
+(function (Color1) {
+    Color1[Color1["red"] = 0] = "red";
+    Color1[Color1["green"] = 6] = "green";
+    Color1[Color1["blue"] = 7] = "blue";
+    Color1[Color1["orange"] = 1] = "orange";
+    Color1[Color1["black"] = 2] = "black";
+})(Color1 || (Color1 = {}));
+var red1 = Color1.red;
+var green1 = Color1.green;
+var blue1 = Color1.blue;
+var orange1 = Color1.orange;
+var black1 = Color1.black;
+console.log(red1); // 0
+console.log(green1); // 6
+console.log(blue1); // 7
+console.log(orange1); // 1
+console.log(black1); // 2
